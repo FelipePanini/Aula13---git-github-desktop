d111ec4215894dce2884b57ea7a2a9579cc0b766
FelipePanini <felipepanini08@gmail.com>
2026-05-07 21:45:47 -0300

    02 - 21/45

--- calc.py
+++ calc.py
@@ -13,3 +13,9 @@ def divisao (a, b):
     if b == 0:
         return "Erro: Divisão por zero não é permitida."
     return (a / b)
+
+def potencia (a, b):
+    return (a ** b)
+
+def raiz_quadrada (a):
+    return (a ** 0.5)
